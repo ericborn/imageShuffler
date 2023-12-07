e0498ed6e1dab81b180517de422f6e4d1161eb66
Eric Born <eric.born85@gmail.com>
2023-12-07 12:55:02 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -16,42 +16,45 @@ path = os.path.dirname(os.path.realpath(__file__))
 # 2 lists, one used one not. on load move images into used and refresh to check for new files to put into not used. Load 8 from not used.
 # once a threshold of not used is reached, ie. less than 8 images remaining, reset the used list.
 
+original_list = []
 
-def create_shuffle_list():
-    result_list = []
+def regen_list():
+    global original_list
+
     items_per_sublist = 8
-
     all_images = [os.path.basename(image) for image in glob.glob(path+'\\static\\*.png')]
 
-    for image in range(24):
+    for image in range(10):
         selected_items = random.sample(all_images, items_per_sublist)
-        result_list.append(selected_items)
+        original_list.append(selected_items)
+    print("regen list")
+    #return(original_list)
 
-    return(result_list)
+def manage_shuffle_list():
+    global original_list
 
-
-def revise_shuffle_list(original_list):
-    if len(original_list) == 0:
-        original_list = create_shuffle_list()
-        print("revise shuffle result")
-        return(original_list)
-    
+    if len(original_list) <= 1:
+        regen_list()
+        #return(original_list)
     else:
         original_list.pop(0)
-        print("revise shuffle original")
-        return(original_list)
+        print("reuse original list")
+        #return(original_list)
 
-image_list = create_shuffle_list()
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', image_list=image_list[0])
+    global original_list
+    manage_shuffle_list()
+    return render_template('index.html', image_list=original_list[0]) #image_list=image_list[0])
 
 # route called from JS which shuffles
 @app.route('/get_images')
 def get_images():
-    result_list = revise_shuffle_list(image_list)
-    return (result_list[0])
+    global original_list
+    manage_shuffle_list()
+    return (original_list[0])
 
 if __name__ == '__main__':
     app.run(debug=True)
